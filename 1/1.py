d11a9164f1b0cd4b2016718d34fd4cffe3fa63da
Hayden Price <hwplivnlow@gmail.com>
2023-12-10 13:08:00 -0700

    initialize first day's file, comment pseudocode

---
+++ 1/1.py
@@ -0,0 +1,6 @@
+# for every line in input text doc
+#   get first and last digits
+#       same if only one digit
+#   concatenate first and last digits
+#   add to sum
+# print sum and copy into browser
